4ec13c61854b457692e3f86431c2a5d5b8b18c13
SeaChan <1341914555@qq.com>
2021-08-18 14:05:28 +0800

    config eslint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,10 @@
 
   <p>
     Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    <a
+      href="https://code.visualstudio.com/"
+      target="_blank"
+    >VSCode</a>
     +
     <a
       href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
@@ -12,7 +15,12 @@
       Vetur
     </a>
     or
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    <a
+      href="https://github.com/johnsoncodehk/volar"
+      target="_blank"
+    >
+      Volar
+    </a>
     (if using
     <code>&lt;script setup&gt;</code>)
   </p>
@@ -20,14 +28,25 @@
   <p>See <code>README.md</code> for more information.</p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+    <a
+      href="https://vitejs.dev/guide/features.html"
+      target="_blank"
+    >
       Vite Docs
     </a>
     |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+    <a
+      href="https://v3.vuejs.org/"
+      target="_blank"
+    >Vue 3 Docs</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <button
+    type="button"
+    @click="count++"
+  >
+    count is: {{ count }}
+  </button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
@@ -36,6 +55,7 @@
 
 <script lang="ts">
 import { ref, defineComponent } from 'vue'
+
 export default defineComponent({
   name: 'HelloWorld',
   props: {
