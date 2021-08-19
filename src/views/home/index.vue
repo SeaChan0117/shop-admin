54ddffb15823aa95ad2bbe469fbb9411f91ab8ca
SeaChan <1341914555@qq.com>
2021-08-19 21:52:16 +0800

    feat: 项目引入 element-plus

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,11 +3,20 @@
     首页
   </h1>
   <div>{{ $store.state.count }}</div>
+  <el-button type="primary">
+    测试
+  </el-button>
+  <el-date-picker
+    v-model="value1"
+    type="date"
+    placeholder="选择日期"
+  />
 </template>
 
 <script lang="ts" setup>
 import { useStore } from '@/store'
 const store = useStore()
+const value1 = ''
 console.log(store.state.count)
 </script>
 
